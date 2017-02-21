80ed5442eb03e4cbd96465a05fc2164963c4d775
Steven Popkin <spopkin@user.noreply.github.com>
2017-02-21 12:32:09 -0500

    Convert pdf-rulefind to use Express
    pdf-rulefind now uses Express js.  This will allow me to have it
    serve up web pages as well as provide a rest api by which those pages
    can communicate back with the server to retrieve information, as the
    searches themselves should be conducted server-side.

--- pdf-rulefind/pdf-rulefind.js
+++ pdf-rulefind/pdf-rulefind.js
@@ -1,26 +1,23 @@
-//Attempt to find a specific rule in a pdf file
-//Do note that you will have to handle input validation externally.
+//Import dependencies
+var serveStatic = require('serve-static');
+var express = require('express');
+var fs = require('fs');
 
-var connect=require('connect');
-var serveStatic=require('serve-static');
-/*
-var filename;
-var keywords;
-function handleParams() {
-    if(process.argv.length < 4) {
-        console.log("Usage: " + process.argv[0] + " " + process.argv[1] + " "
-            + "<filename> <keyword to search for> [more keywords]...");
-    }
-    filename = process.argv[2];
-    keywords = [];
-    for (i=3; i < process.argv.length; i++) {
-        keywords.push(process.argv[i]);
-    }
-    console.log("search " + filename + " for " + keywords);
-}
-handleParams();
-*/
+//Read in and parse config file
+var config = JSON.parse(fs.readFileSync('/etc/dmtk/pdf-rulefind-config.json', 'utf8'));
 
-connect().use(serveStatic(__dirname)).listen(8080, function() {
-    console.log("Running on 8080...")
+//Prepare to rest
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    //res.send('Index')
+    res.redirect('/pdf-rulefind.html');
 });
+
+console.log('Started');
+console.log('Using ' + config.ruleDirectory + ' as rulebook directory.');
+
+app.listen(8080, function () {
+      console.log('listening on port 8080')
+})
