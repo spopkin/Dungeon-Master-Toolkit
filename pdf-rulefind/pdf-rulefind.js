e7cc7dbab227ae7ddedaa8e52cdcbd3c53b116ea
Steven Popkin <spopkin@user.noreply.github.com>
2017-03-02 12:46:54 -0500

    Lay groundwork for book listing rules
    Added checks for whitelisting and blacklisting.  They don't
    currently change the list of books returned on the list-books call,
    but the config checks are there.
    
    Also cleaned up the code to remove some hardcoding and commented
    out code.

--- pdf-rulefind/pdf-rulefind.js
+++ pdf-rulefind/pdf-rulefind.js
@@ -2,11 +2,17 @@
 var serveStatic = require('serve-static');
 var express = require('express');
 var fs = require('fs');
-var ruleBookDir = "/usr/share/dmtk/rulebooks";
 var configFile = '/etc/dmtk/pdf-rulefind-config.json';
+var ruleBookDirDefault = "/usr/share/dmtk/rulebooks";
+var ruleBookDir = ruleBookDirDefault;
 
 //Read in and parse config file
 var config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+ruleBookDirConfigured = config.ruleDirectory;
+if (ruleBookDirConfigured != null && ruleBookDirConfigured != "") {
+    console.log("Rulebook directory configured to be: " + ruleBookDirConfigured);
+    ruleBookDir = ruleBookDirConfigured;
+}
 
 //Prepare to rest
 var app = express();
@@ -25,13 +31,27 @@ app.get('/search', function(req, res) {
 
 app.get('/books', function(req, res) {
     fs.exists(ruleBookDir,function(exists){
-        //res.send("Exists");
+        //get the directory contents
         var dirContents = fs.readdirSync(ruleBookDir);
         console.log("Rulebook directory contents:");
         console.log(dirContents);
         var jsonContents = JSON.stringify(dirContents);
         res.send(jsonContents);
-        // handle result
+
+        //is book whitelisting enabled
+        if (config.bookWhitelist) {
+            console.log("allowed books are whitelisted");
+            console.log("whitelist: " + config.whiteList);
+        } 
+        //is book blacklisting enabled
+        if (config.bookBlackList) {
+            console.log("disallowed books are blacklisted");
+            console.log("blackList: " + config.blackList);
+        }
+        //are dungeon masters immune to black and white listing of books?
+        if (config.dmsOverrideBlackWhiteLists) {
+            console.log("game masters can see all books");
+        }
     });
 });
 
